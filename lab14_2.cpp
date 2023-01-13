2d93c365a9771bcdde64da4f30aaf6bc1d4a4a36
= <=>
2023-01-13 17:16:38 +0700

    lab14.2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,41 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[N][M],int s,int x,int y){
+    int i=0;
+    int j=0;
+    while(i<30){
+        j=0;
+        while(j<70){
+            if(sqrt(pow((i-x),2)+pow((j-y),2))<=s-1){
+                image[i][j]=1;
+            }
+            j++;
+        }
+
+    i++;
+    }
+}
+
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------\n";
+    int i=0;
+    int j=0;
+    while(i<30){
+        cout <<"|";
+        j=0;
+        while(j<70){
+
+            if(image[i][j]==1){
+                cout <<"*";
+            }else{
+                cout <<" ";
+            }
+            j++;
+        }
+        cout <<"|\n";
+    i++;
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
